a32e4e58657528bc5d9c4f37eb4ed3bc1704e6cf
AyurDondokov <ayur00dondokov@gmail.com>
2022-12-16 02:15:39 +0800

    1

--- TaskManager/scripts/app.js
+++ TaskManager/scripts/app.js
@@ -1,59 +1,61 @@
 
 (function () {
-    let todolist = document.querySelector('.todo-list');
-    let addButton = todolist.querySelector('.task-list__add-button');
+    let todoList = document.querySelector('.todo-list');
+    let addButton = todoList.querySelector('.task-list__add-button');
 
-    let taskList = todolist.querySelector('task-list');
-    let taskForm = todolist.querySelector('.task-form');
-    let form = taskForm.querySelector('form')
+    let taskList = todoList.querySelector('.task-list');
+    let taskForm = todoList.querySelector('.task-form');
+    let form = taskForm.querySelector('form');
     let closeButton = taskForm.querySelector('.task-form__close');
 
-    let formAction = '';
+    let formAction = 'create';
     let formItem;
 
     let editAction = e => {
+        let btn = e.target;
         let item = btn.closest('.task-list__item');
-        let prio = item.querySelector('.task-list__prio');
-        let desc = item.querySelector('.task-list__description');
+        let prio = item.querySelector('.task-list__prio')
+        let desc = item.querySelector('.task-list__description')
         form.reset();
         form.description.value = desc.textContent;
         form.prio.checked = (prio.textContent !== '');
         taskForm.classList.add('shown');
         formAction = 'edit';
-        formItem = item
-    }
+        formItem = item;
+    };
+
+    let delAction = e => {
+        let btn = e.target;
+        let item = btn.closest('.task-list__item');
+        item.remove();
+    };
 
     form.addEventListener('submit', e => {
         e.preventDefault();
         switch (formAction){
             case 'create':
-                // let item = document.createElement('li');
+                //let item = document.createElement('li');
                 let item = taskList.firstElementChild.cloneNode(true);
+                item.style.display = '';
                 item.querySelector('.task-list__done').checked = false;
-                item.querySelector('.task-description').textContent = form.description.value;
-                item.querySelector('.task-list__prio').textContent =
-                    form.prio.checked ? '☻' : '';
+
+                item.querySelector('.task-list__description').textContent = form.description.value;
+                item.querySelector('.task-list__prio').textContent = form.prio.checked ? '!!!' : '';
                 taskList.append(item);
-                item.addEventListener('click', editAction);
+                item.querySelector('.task-list__edit-button').addEventListener('click', editAction);
+                item.querySelector('.task-list__del-button').addEventListener('click', delAction);
                 taskForm.classList.toggle('shown');
                 form.reset();
                 break;
             case 'edit':
                 formItem.querySelector('.task-list__done').checked = false;
-                formItem.querySelector('.task-description').textContent = form.description.value;
-                formItem.querySelector('.task-list__prio').textContent =
-                    form.prio.checked ? '☻' : '';
-                taskList.append(item);
-                item.addEventListener('click', editAction);
-                taskForm.classList.toggle('shown');
-                form.reset();
+                formItem.querySelector('.task-list__description').textContent = form.description.value;
+                formItem.querySelector('.task-list__prio').textContent = form.prio.checked ? '!!!' : '';
                 break;
-
         }
-    });
+    })
 
     addButton.addEventListener('click', e => {
-        taskForm.classList.toggle('shown');
         formAction = 'create';
         form.reset();
         formItem = undefined;
@@ -63,18 +65,10 @@
         taskForm.classList.remove('shown');
     });
 
-    let editButton = todolist.querySelectorAll('.task-list__edit-button');
-    editButton.forEach(btn => {
-        btn.addEventListener('click', e => {
-            let item = btn.closest('.task-list__item');
-            let prio = item.querySelector('.task-list__prio');
-            let desc = item.querySelector('.task-list__description');
-            form.reset();
-            form.description.value = desc.textContent;
-            form.prio.checked = (prio.textContent !== '');
-            taskForm.classList.add('shown');
-            formAction = 'edit';
-        });
+    let editButtons = todoList.querySelectorAll('.task-list__edit-button');
+
+    editButtons.forEach(btn => {
+        btn.addEventListener('click', editAction);
     });
 
 })();
